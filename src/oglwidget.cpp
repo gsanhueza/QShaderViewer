1a27b219293079c760cb550c1f88675112d5a5bd
gsanhueza <gabriel_8032@hotmail.com>
2017-09-13 22:28:02 -0300

    Ultimo intento con cmake

--- src/oglwidget.cpp
+++ src/oglwidget.cpp
@@ -21,25 +21,25 @@ void OGLWidget::initializeGL()
 
 void OGLWidget::paintGL()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-        glColor3f ( 1.0,  0.0,  0.0);
-        glVertex3f(-0.5, -0.5,  0.0);
-        glColor3f ( 0.0,  1.0,  0.0);
-        glVertex3f( 0.5, -0.5,  0.0);
-        glColor3f ( 0.0,  0.0,  1.0);
-        glVertex3f( 0.0,  0.5,  0.0);
-    glEnd();
+//     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//     glBegin(GL_TRIANGLES);
+//         glColor3f ( 1.0,  0.0,  0.0);
+//         glVertex3f(-0.5, -0.5,  0.0);
+//         glColor3f ( 0.0,  1.0,  0.0);
+//         glVertex3f( 0.5, -0.5,  0.0);
+//         glColor3f ( 0.0,  0.0,  1.0);
+//         glVertex3f( 0.0,  0.5,  0.0);
+//     glEnd();
 }
 
 void OGLWidget::resizeGL(int w, int h)
 {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45, (float) w / h, 0.01, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+//     glViewport(0, 0, w, h);
+//     glMatrixMode(GL_PROJECTION);
+//     glLoadIdentity();
+//     gluPerspective(45, (float) w / h, 0.01, 100.0);
+//     glMatrixMode(GL_MODELVIEW);
+//     glLoadIdentity();
+//     gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
 }
