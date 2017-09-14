e69997136350d0a133aa9e97a46b4d7a25dd80ac
gsanhueza <gabriel_8032@hotmail.com>
2017-09-14 14:57:57 -0300

    Agregando lógica al modelo v0

--- src/model.cpp
+++ src/model.cpp
@@ -10,15 +10,18 @@ Model::~Model()
 
 bool Model::loadGeometry(string filepath)
 {
+    cout << "TODO: Model - Cargando geometría... (" << filepath << ")" << endl;
     return true;
 }
 
 bool Model::loadVertexShader(string filepath)
 {
+    cout << "TODO: Model - Cargando vertex... (" << filepath << ")" << endl;
     return true;
 }
 
 bool Model::loadFragmentShader(string filepath)
 {
+    cout << "TODO: Model - Cargando fragment... (" << filepath << ")" << endl;
     return true;
 }
--- src/shaderviewer.cpp
+++ src/shaderviewer.cpp
@@ -19,14 +19,17 @@ ShaderViewer::~ShaderViewer()
 void ShaderViewer::loadGeometryClicked()
 {
     std::cout << "Test loadGeometryClicked()" << std::endl;
+    m_model.loadGeometry("TODO: filepath va aquí");
 }
 
 void ShaderViewer::loadVertexClicked()
 {
     std::cout << "Test loadVertexClicked()" << std::endl;
+    m_model.loadVertexShader("TODO: filepath va aquí");
 }
 
 void ShaderViewer::loadFragmentClicked()
 {
     std::cout << "Test loadFragmentClicked()" << std::endl;
+    m_model.loadFragmentShader("TODO: filepath va aquí");
 }
