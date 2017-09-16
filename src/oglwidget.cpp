1f7d3531f5d06cf33ab3fc56b913dd1aa5d0709d
gsanhueza <gabriel_8032@hotmail.com>
2017-09-16 12:24:51 -0300

    Respaldo de shader, se espera ver rojo

--- src/oglwidget.cpp
+++ src/oglwidget.cpp
@@ -32,7 +32,7 @@ static const char *fragmentShaderSource =
 "   highp vec3 L = normalize(lightPos - vert);\n"
 "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
 "   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
-"   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n""
+"   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
 "   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
 // "   gl_FragColor = vec4(col, 1.0);\n"
 "}\n";
@@ -76,7 +76,7 @@ void OGLWidget::initializeGL()
     m_vbo.create();
     m_vbo.bind();
     // FIXME Parece que aquí se ponen los vértices de mi polígono
-    m_data.append(QVector<GLfloat>{0.5, 0.0, 0.0});
+    m_data.append(QVector<GLfloat>{0.5, 0.0, -0.05});
     m_data.append(QVector<GLfloat>{0.0, 0.5, 0.0});
     m_data.append(QVector<GLfloat>{-0.5, 0.0, 0.0});
 
