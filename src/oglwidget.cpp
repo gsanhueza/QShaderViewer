8db5091f88ec6baecf80c3fe9c08a5c6be240396
gsanhueza <gabriel_8032@hotmail.com>
2017-09-16 12:19:08 -0300

    Portando a un opengl más moderno

--- src/oglwidget.cpp
+++ src/oglwidget.cpp
@@ -1,7 +1,8 @@
 #include "oglwidget.h"
 
 OGLWidget::OGLWidget(QWidget* parent)
-    : QOpenGLWidget(parent)
+    : QOpenGLWidget(parent),
+      m_program(0)
 {
 }
 
@@ -9,45 +10,118 @@ OGLWidget::~OGLWidget()
 {
 }
 
+static const char *vertexShaderSource =
+"attribute vec4 vertex;\n"
+"attribute vec3 normal;\n"
+"varying vec3 vert;\n"
+"varying vec3 vertNormal;\n"
+"uniform mat4 projMatrix;\n"
+"uniform mat4 mvMatrix;\n"
+"uniform mat3 normalMatrix;\n"
+"void main() {\n"
+"   vert = vertex.xyz;\n"
+"   vertNormal = normalMatrix * normal;\n"
+"   gl_Position = projMatrix * mvMatrix * vertex;\n"
+"}\n";
+
+static const char *fragmentShaderSource =
+"varying highp vec3 vert;\n"
+"varying highp vec3 vertNormal;\n"
+"uniform highp vec3 lightPos;\n"
+"void main() {\n"
+"   highp vec3 L = normalize(lightPos - vert);\n"
+"   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
+"   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
+"   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n""
+"   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+// "   gl_FragColor = vec4(col, 1.0);\n"
+"}\n";
+
+void OGLWidget::setupVertexAttribs()
+{
+    m_vbo.bind();
+    QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+    f->glEnableVertexAttribArray(0);
+    f->glEnableVertexAttribArray(1);
+    f->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
+    f->glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<void *>(3 * sizeof(GLfloat)));
+    m_vbo.release();
+}
+
 void OGLWidget::initializeGL()
 {
-    glClearColor(0, 0, 0, 1);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHTING);
-    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-    glEnable(GL_COLOR_MATERIAL);
+    initializeOpenGLFunctions();
+
+    m_program = new QOpenGLShaderProgram;
+    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    m_program->bindAttributeLocation("vertex", 0);
+    m_program->bindAttributeLocation("normal", 1);
+    m_program->link();
+
+    m_program->bind();
+    m_projMatrixLoc = m_program->uniformLocation("projMatrix");
+    m_mvMatrixLoc = m_program->uniformLocation("mvMatrix");
+    m_normalMatrixLoc = m_program->uniformLocation("normalMatrix");
+    m_lightPosLoc = m_program->uniformLocation("lightPos");
+
+    // Create a vertex array object. In OpenGL ES 2.0 and OpenGL 2.x
+    // implementations this is optional and support may not be present
+    // at all. Nonetheless the below code works in all cases and makes
+    // sure there is a VAO when one is needed.
+    m_vao.create();
+    QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
+
+    // Setup our vertex buffer object.
+    m_vbo.create();
+    m_vbo.bind();
+    // FIXME Parece que aquí se ponen los vértices de mi polígono
+    m_data.append(QVector<GLfloat>{0.5, 0.0, 0.0});
+    m_data.append(QVector<GLfloat>{0.0, 0.5, 0.0});
+    m_data.append(QVector<GLfloat>{-0.5, 0.0, 0.0});
+
+    m_vbo.allocate(m_data.constData(), m_data.count() * sizeof(GLfloat));
+
+    // Store the vertex attribute bindings for the program.
+    setupVertexAttribs();
+
+    // Our camera never changes in this example.
+    m_camera.setToIdentity();
+    m_camera.translate(0, 0, -1);
+
+    // Light position is fixed.
+    m_program->setUniformValue(m_lightPosLoc, QVector3D(0, 0, 70));
+
+    m_program->release();
 }
 
 void OGLWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
 
-    glBegin(GL_POLYGON);
+    m_world.setToIdentity();
+//     m_world.rotate(180.0f - (m_xRot / 16.0f), 1, 0, 0);
+//     m_world.rotate(m_yRot / 16.0f, 0, 1, 0);
+//     m_world.rotate(m_zRot / 16.0f, 0, 0, 1);
 
-    srand(time(0));
+    QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
+    m_program->bind();
+    m_program->setUniformValue(m_projMatrixLoc, m_proj);
+    m_program->setUniformValue(m_mvMatrixLoc, m_camera * m_world);
+    QMatrix3x3 normalMatrix = m_world.normalMatrix();
+    m_program->setUniformValue(m_normalMatrixLoc, normalMatrix);
 
-    for (vector<float> point : m_model.getCoordinates())
-    {
-        float R = static_cast<double>(rand()) / RAND_MAX;
-        float G = static_cast<double>(rand()) / RAND_MAX;
-        float B = static_cast<double>(rand()) / RAND_MAX;
-        glColor3f (R, G, B);
-        glVertex3f(point.at(0) / 100, point.at(1) / 100, point.at(2) / 100);
-    }
-    glEnd();
-    this->update();
+    glDrawArrays(GL_TRIANGLES, 0, m_data.count() / 3);
+
+    m_program->release();
 }
 
 void OGLWidget::resizeGL(int w, int h)
 {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-//     gluPerspective(45, (float) w / h, 0.01, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-//     gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+    m_proj.setToIdentity();
+    m_proj.perspective(45.0f, GLfloat(w) / h, 0.01f, 100.0f);
 }
 
 void OGLWidget::receiveModel(const Model &m)
--- src/oglwidget.h
+++ src/oglwidget.h
@@ -2,13 +2,18 @@
 #define _OGLWIDGET_H_
 
 #include <QWidget>
+#include <QOpenGLBuffer>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLVertexArrayObject>
 #include <QOpenGLWidget>
+#include <QMatrix4x4>
 #include <GL/glu.h>
 #include <GL/gl.h>
 
 #include "model.h"
 
-class OGLWidget : public QOpenGLWidget
+class OGLWidget : public QOpenGLWidget, protected QOpenGLFunctions
 {
     Q_OBJECT;
 
@@ -26,5 +31,20 @@ protected:
 
 private:
     Model m_model;
+
+    void setupVertexAttribs();
+
+    QOpenGLVertexArrayObject m_vao;
+    QOpenGLBuffer m_vbo;
+    QOpenGLShaderProgram *m_program;
+    int m_projMatrixLoc;
+    int m_mvMatrixLoc;
+    int m_normalMatrixLoc;
+    int m_lightPosLoc;
+    QMatrix4x4 m_proj;
+    QMatrix4x4 m_camera;
+    QMatrix4x4 m_world;
+
+    QVector<GLfloat> m_data;
 };
 #endif
