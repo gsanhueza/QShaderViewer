15dd7419da8c3c5ed1bb709460ac2a3168833abc
gsanhueza <gabriel_8032@hotmail.com>
2017-09-17 00:49:05 -0300

    Reparando shaders, probablemente hay que hacer calzar los uniforms de los shaders con datos de oglwidget

--- data/PhongFragment.glsl
+++ data/PhongFragment.glsl
@@ -1,6 +1,6 @@
 varying vec4 myColor;
-varying vec3 vertNormal;
-varying vec3 vert;
+varying vec3 myNormal;
+varying vec3 myPosition;
 
 uniform vec3 lightPos;
 uniform vec3 lightColSun;
@@ -14,15 +14,14 @@ void main() {
     rotation[1] = vec3(-1, 0, 0);
     rotation[2] = vec3(0, 0, 1);
 
-    vec3 LightVectorSun = rotation * normalize(lightPos - vert);
-//     vec3 ReflectedVectorSun = reflect(LightVectorSun, vertNormal);
-    vec3 EyeVector = -normalize(vert);
+    vec3 LightVectorSun = rotation * normalize(lightPos - myPosition);
+    vec3 ReflectedVectorSun = reflect(LightVectorSun, myNormal);
+    vec3 EyeVector = -normalize(myPosition);
 
-    float IdiffSun = max(dot(vertNormal, LightVectorSun), 0.0); // diffuse
-    float IspecSun = 0.0; // pow(max(dot(EyeVector, ReflectedVectorSun), 0.0), brightness); // specular
+    float IdiffSun = max(dot(myNormal, LightVectorSun), 0.0); // diffuse
+    float IspecSun = pow(max(dot(EyeVector, ReflectedVectorSun), 0.0), brightness); // specular
     float IambSun = 0.2; // ambient
 
     vec3 colorSun = blend * lightColSun + (1 - blend) * myColor;
-    vec3 colorOrbital = blend * lightColOrbital + (1 - blend) * myColor;
     gl_FragColor = vec4(colorSun * (IdiffSun + IspecSun + IambSun), myColor.a), myColor.a);
 }
