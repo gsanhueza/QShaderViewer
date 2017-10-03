66855758967c5ae1e5214079d57eb4c0d5913959
gsanhueza <gabriel_8032@hotmail.com>
2017-10-03 15:19:09 -0300

    WIP: Implementando los valores fresnel, roughness y geometric

--- data/PBRCookTorranceFragment.glsl
+++ data/PBRCookTorranceFragment.glsl
@@ -10,7 +10,7 @@ For performance reasons, in real-time 3D graphics Schlick's approximation is oft
 
 G is the geometric attenuation term, describing selfshadowing due to the microfacets, and is of the form
 
-D = TODO
+D = (1 / (m^2 * (N · H)^4)) * exp((N · H)^2 - 1 / (m^2 * (N · H)^2)     // Beckmann
 F = TODO
 G = min {(1, 2(H · N)(V · N) / (V · H), 2(H · N)(L · N) / (V · H)}      // These are 3 components inside min.
 
@@ -29,14 +29,22 @@ varying highp vec3 vertNormal;
 uniform highp vec3 lightPos;
 uniform highp vec3 eyePos;
 
-float beckmannDistribution(vec3 H)
+float beckmannDistribution(vec3 N, vec3 H, float m)
 {
-    return 0.0;
+    // D = (1 / (m^2 * (N · H)^4)) * exp((N · H)^2 - 1 / (m^2 * (N · H)^2)
+    float NdotH = max(0.0, dot(N, H));
+
+    float d_frac = 1.0 / (pow(m, 2) * pow(NdotH, 4));
+    float d_exp = exp((pow(NdotH, 2) - 1) / (pow(m, 2) * pow(NdotH, 2)));
+    return d_frac * d_exp;
 }
 
-float fresnelValue(vec3 L, vec3 H)
+float fresnelValue(vec3 H, vec3 V, float refIdx)
 {
-    return 0.0;
+    // F = F0 + (1 - (H · V))^5 * (1 - F0);
+    float HdotV = max(0.0, dot(H, V));
+
+    return refIdx + pow(1 - HdotV, 5) * (1 - refIdx);
 }
 
 float geometricAttenuation(vec3 L, vec3 N, vec3 V, vec3 H)
@@ -62,29 +70,32 @@ void main()
 {
     float brightness = 100.0;
     vec3 lightCol = vec3(0.5, 0.5, 0.0);
-
-    // Geometric Term
     vec3 HalfVector = normalize(lightPos + eyePos);
     vec3 LightVector = normalize(lightPos - vert);
     vec3 EyeVector = normalize(eyePos - vert);
 
-    float geo = geometricAttenuation(LightVector, vertNormal, EyeVector, HalfVector);
+    float NdotL = max(0.0, dot(vertNormal, LightVector));
+    float NdotH = max(0.0, dot(vertNormal, HalfVector));
+    float NdotV = max(0.0, dot(vertNormal, EyeVector));
+    float VdotH = max(0.0, dot(EyeVector, HalfVector));
 
-    // TODO Compute roughness (Beckmann distribution, a.k.a. D)
-    float roughness = beckmannDistribution(HalfVector);
+    // Geometric Term
 
-    // TODO Compute Fresnel term
-    float fresnel = fresnelValue(LightVector, HalfVector);
+    float geometric = geometricAttenuation(LightVector, vertNormal, EyeVector, HalfVector);
 
-    vec3 ReflectedVectorSun = reflect(LightVector, vertNormal);
+    // FIXME Get roughness value (uniform float)
+    float roughnessValue = 0.1;
+    float roughness = beckmannDistribution(vertNormal, HalfVector, roughnessValue);
 
+    // FIXME Get refraction index for Fresnel term (uniform float)
+    float refractionIndex = 0.2;
+    float fresnel = fresnelValue(HalfVector, EyeVector, refractionIndex);
 
     // Diffuse
     float Idiff = max(dot(vertNormal, LightVector), 0.0);
 
-    // TODO Evaluate specular with the above data
     // Specular
-    float Ispec = pow(max(dot(EyeVector, ReflectedVectorSun), 0.0), brightness);
+    float Ispec = (fresnel * roughness * geometric) / (NdotV * NdotL) ;
 
     // Return whole illumination as diffuse + specular
     gl_FragColor = vec4(lightCol * (Idiff + Ispec), 1.0);
