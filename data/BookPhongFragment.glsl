c924440532f35ee34531ea64f405556c4345029d
gsanhueza <gabriel_8032@hotmail.com>
2017-10-03 00:27:21 -0300

    Comprobación de libro para Phong: Cálculos correctos

---
+++ data/BookPhongFragment.glsl
@@ -0,0 +1,26 @@
+#version 330 core
+
+varying highp vec3 vert;
+varying highp vec3 vertNormal;
+
+uniform highp vec3 lightPos;
+uniform highp vec3 eyePos;
+
+void main() {
+    float brightness = 100.0;
+    vec3 lightColSun = vec3(0.5, 0.5, 0.0);
+    vec3 LightVectorSun = normalize(lightPos - vert);
+    vec3 EyeVector = normalize(eyePos - vert);
+
+    vec3 ReflectedVectorSun = normalize(2.0 * vertNormal * dot(vertNormal, LightVectorSun) - LightVectorSun);
+    float RdotV = max(0.0, dot(ReflectedVectorSun, EyeVector));
+
+
+    // Diffuse
+    float Idiff = max(dot(vertNormal, LightVectorSun), 0.0);
+
+    // Specular
+    float Ispec = pow(RdotV, brightness);
+
+    gl_FragColor = vec4(lightColSun * (Idiff + Ispec), 1.0);
+}
